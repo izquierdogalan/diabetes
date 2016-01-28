e44f78f6e9ba9523946b67eddee55f4373e5bc25
Jonathan Izquierdo Galán <jonathan.izquierdo@fon.com>
2016-01-28 12:34:41 +0100

    Delete test

--- src/AppBundle/Tests/Services/RestClientTest.php
+++ src/AppBundle/Tests/Services/RestClientTest.php
@@ -18,43 +18,8 @@ class RestClientTest extends WebTestCase
 
     public function testLogin()
     {
-        $client = new RestClient($this->container);
-//        $client = $this->container->get('app.services.rest_client');
-        $response = $client->login('admin', 'admin');
-//
-//        $crawler = $client->request('GET', '/');
-//
-//        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-//        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+
     }
 
-
-//    /**
-//     * Create a client with a default Authorization header.
-//     *
-//     * @param string $username
-//     * @param string $password
-//     *
-//     * @return \Symfony\Bundle\FrameworkBundle\Client
-//     */
-//    protected function createAuthenticatedClient($username = 'adminuser', $password = '151')
-//    {
-//        $client = static::createClient();
-//        $client->request(
-//            'POST',
-//            '/api/login_check',
-//            array(
-//                '_username' => $username,
-//                '_password' => $password,
-//            )
-//        );
-//
-//        $data = json_decode($client->getResponse()->getContent(), true);
-//
-//        $client = static::createClient();
-//        $client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
-//
-//        return $client;
-//    }
 
 }
